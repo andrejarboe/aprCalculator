a3eb11fafba7ba1639a6c7d1dc4ba4eb7a6e2702
andrejarboe <andrejarboe@gmail.com>
2021-08-03 20:40:25 -0400

    fixed form methon and refatored api to a folder

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const path = require('path');
-
+const router = express.Router();
 
 const app = express();
 
-
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Mortgage api 
+app.use('/api/mortgage', require('./routes/api/mortgage'))
 
 const PORT = process.env.PORT || 5000;
 
---
+++ routes/api/mortgage.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+// Calculate mortgage
+router.post('/', (req, res) => {
+    console.log('hello');
+    
+});
+
+module.exports = router;
