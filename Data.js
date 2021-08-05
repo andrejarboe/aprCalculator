ce9e740982ea28972faddd176babe3a8a1a80ae9
andrejarboe <andrejarboe@gmail.com>
2021-08-04 22:06:12 -0400

    fixed forumla so it uses the monthly intrest rate not the yearly

--- Data.js
+++ Data.js
@@ -1,8 +1,9 @@
 let data = {
-    principal: '',
-    term: '',
-    interest: '',
-    mortgage: ''
+    principal: 200000,
+    term: 30,
+    interest: 4.5,
+    monthly: 1013.37,
+    apr: 4.703
 }
 
 module.exports = data;
--- routes/api/mortgage.js
+++ routes/api/mortgage.js
@@ -6,11 +6,6 @@ const data = require("../../Data");
 router.post("/", (req, res) => {
   console.log("hello");
 
-  // console.log(req.body);
-  // console.log(mortgage);
-  // console.log(term);
-  // console.log(interest);
-
   // want to show the monthy payments
   // ex 300,000 5% 30years
   // m = (p * r (1+r)^n)/((1+r)^n -1)
@@ -22,15 +17,60 @@ router.post("/", (req, res) => {
   // r = 5 /100 = 0.05
   // m = 1,610.46
 
-  const p = req.body.mortgage;
-  const n = req.body.term * 12;
-  const r = req.body.interest / 100;
-  const m = (p * r * (1 + r) ** n) / ((1 + r) ** n - 1);
+  function calculateData(data) {
+    const totalMonths = data.term * 12;
+    const monthlyInterest = (data.interest / 100) / 12;
+    const monthlyPrincipalInterest =
+    //   data.principal *
+    //   ((monthlyInterest * (1 + monthlyInterest) ** totalMonths) /
+    //     ((1 + monthlyInterest) ** totalMonths - 1));
+        data.principal *
+        (
+            ( monthlyInterest * ((1 + monthlyInterest )**totalMonths)) /
+            ((1+monthlyInterest)**totalMonths - 1)
+        )   
+        console.log("*******monthlyPrincipalInterest********");
+        console.log(monthlyPrincipalInterest);
+        console.log("*******monthlyPrincipalInterest********");
 
-  data.mortgage = m;
+    data.monthly = monthlyPrincipalInterest;
+  }
 
-  console.log("M::::::");
-  console.log("M::::::" + m);
+  
+  console.log("*******old********");
+  console.log(data);
+  console.log("*******old********");
+  
+  //   set data state
+  data.principal = Number(req.body.principal);
+  data.term = Number(req.body.term);
+  data.interest = Number(req.body.interest);
+  
+  calculateData(data);
+
+  console.log("*******New********");
+  console.log(data);
+  console.log("*******New********");
+
+//   const p = data.principal;
+//   console.log("Mortgage: " + p);
+//   const t = data.term * 12;
+//   console.log("Terms: " + t);
+//   const i = data.interest / 100;
+//   console.log("Interest: " + i);
+//   // p * ( (i * (1+i)**t )/ ((1+i)**t) - 1)
+
+  //   const m = p * (i * (1 + i)** t) / ((1 + i)** t - 1);
+
+  // let m = ((1+i)**t )- 1
+//   let m = (p * (i * (1 + i) ** t)) / ((1 + i) ** t - 1);
+//   console.log("Monthly " + m);
+
+  //   data.principal = p;
+  //   data.term = t;
+  //   data.intrest =
+  
+
   console.log("***************");
   console.log(data);
   console.log("***************");
